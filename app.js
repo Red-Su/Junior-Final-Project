c91489c0d6b6fbe75696f1dd2459173d5991b298
Red-Su <93939386+Red-Su@users.noreply.github.com>
2022-05-24 09:41:20 -0700

    Finish

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 function test() {
     document.getElementById("explode").style.visibility = "visible";
+    document.getElementById("text").style.color = "black";
+    document.getElementById("text").style.textShadow = "1px 1px black";
+    document.getElementById("text").style.color = "black";
 }
 function random() {
-    var x =  Math.floor(Math.random() * 16);
+    var x =  Math.floor(Math.random() * 15);
     switch (x) {
         case 0: 
             return "A good magician's assistant is hard to find. They're highly sawed after."
@@ -13,28 +16,26 @@ function random() {
         case 3:
             return "The magician got so angry he pulled his hair out."
         case 4:
-            return "Harry Houdini used to use lots of trap doors in his magic act. He's stopped now, he was just going through a stage."         
-        case 5:
             return "The chocolate magician performed a lot of Twix."   
-        case 6:
+        case 5:
             return `I asked why the magician was late to the party. He said, "bewitcha in a minute!"`
-        case 7:
+        case 6:
             return "I never saw the magician's spell book. He kept it under warlock and key."
-        case 8:
+        case 7:
             return "The magician was worried about his garden after the summer. He'd been going through a dry spell."
-        case 9:
+        case 8:
             return "Last week I saw a magician walk down the street. He turned into a drugstore."
-        case 10:
+        case 9:
             return "If I want to be a good magician I really have to pocus on my craft."
-        case 11:
+        case 10:
             return "The witch hated riding her broomstick on a cold night. She just went out for a short spell."
-        case 12:
+        case 11:
             return `There's a sign outside the witch's house on my street. "Witches parking only! All others will be toad."`
-        case 13:
+        case 12:
             return "I went to witch school when I was a kid. I didn't love it, all we did was spelling."
-        case 14:
+        case 13:
             return "Be careful of angry witches. They tend to fly off the handle."
-        case 15:
+        case 14:
             return "Witches don't really like martial arts. The only exception is taekwando."
     }
 }
